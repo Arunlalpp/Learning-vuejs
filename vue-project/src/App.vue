441be796b0ad59fc831e43fa5db399c8afc25288
Arunlal PP <arunlalpp@gmail.com>
2024-03-05 12:45:21 +0530

    feat: show specific names from an array using v-for and v-if

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,47 +4,15 @@ export default {
   data() {
     return {
       names: ["Arunlal", "pcp", "20sil_pcp"],
-      fullNames: [
-        { first: "Arun", last: "lal" },
-        { first: "Nidheesh", last: "Kunuvava" },
-        { first: "Sree", last: "kutty" },
-      ],
-      actors: [
-        {
-          name: "Dileep",
-          movies: ["vinodayathra", "CID moosa"],
-        },
-        {
-          name: "Harisree ashokan",
-          movies: ["Panjabi house", "CID moosa"],
-        },
-      ],
-      myInfo: {
-        name: "Arunlal",
-        channel: "Codevelution",
-        course: "Vue 3",
-      },
     };
   },
 };
 </script>
 
 <template>
-  <h2 v-for="(name, index) in names" :key="name">{{ index }}{{ name }}</h2>
-  <h2 v-for="name in fullNames" :key="name.first">
-    {{ name.first }} {{ name.last }}
-  </h2>
-  <div v-for="actor in actors" :key="actor.name">
-    <h2>{{ actor.name }}</h2>
-    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-  </div>
-  <h2 v-for="(value, key, index) in myInfo" :key="value">
-    {{ index }} {{ key }} {{ value }}
-  </h2>
   <template v-for="name in names" :key="name"
-    ><h2>{{ name }}</h2>
-    <hr
-  /></template>
+    ><h2 v-if="name === 'pcp'">{{ name }}</h2></template
+  >
 </template>
 
 <style>
