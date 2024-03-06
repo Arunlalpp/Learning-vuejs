3bbb02b56d2134b1ac07adaca8fe91031ac98584
Arunlal PP <arunlalpp@gmail.com>
2024-03-06 14:41:28 +0530

    feat: emit array and object method

--- vue-components/src/components/Popup.vue
+++ vue-components/src/components/Popup.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h2>This is a popup</h2>
-    <button @click="$emit('close', 'Arunlal')">Close popup</button>
+    <input type="text" name="" id="" v-model="name" />
+    <button @click="$emit('close', name)">Close popup</button>
   </div>
 </template>
 
@@ -9,7 +10,20 @@
 <script>
 export default {
   name: `Popup`,
-  emits: ['close']
+  emits: {
+    close: (name) => {
+      if (!name) {
+        return false
+      } else {
+        return true
+      }
+    }
+  },
+  data() {
+    return {
+      name: ''
+    }
+  }
 }
 </script>
 
