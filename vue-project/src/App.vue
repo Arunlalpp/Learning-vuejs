fd52aec1b16de296d2c011b40585e23368f9d845
Arunlal PP <arunlalpp@gmail.com>
2024-03-06 11:17:29 +0530

    feat: watchers in vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,68 +3,30 @@ export default {
   name: "App",
   data() {
     return {
-      firstName: "Arunlal",
-      lastName: "PP",
-      items: [
-        {
-          id: 1,
-          title: "TV",
-          price: 100,
-        },
-        {
-          id: 2,
-          title: "Phone",
-          price: 200,
-        },
-        {
-          id: 3,
-          title: "Laptop",
-          price: 300,
-        },
-      ],
-      country: "",
+      volume: 0,
     };
   },
-  methods: {
-    getTotal() {
-      console.log("getTotal method");
-      return this.items.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
-    },
-    changeFullName() {
-      this.fullName = "Arunlal PCP";
-    },
-  },
-  computed: {
-    fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`;
-      },
-      set(value) {
-        const names = value.split(" ");
-        this.firstName = names[0];
-        this.lastName = names[1];
-      },
-    },
-    total() {
-      console.log("Total computed property");
-      return this.items?.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
-    },
-    expensiveItems() {
-      return this.items.filter((item) => item.price > 100);
+  methods: {},
+  computed: {},
+  watch: {
+    volume(newValue, oldValue) {
+      if (newValue > oldValue && newValue === 16) {
+        alert(
+          "Listening to hight volume for a long time may damage your gearing"
+        );
+      }
     },
   },
 };
 </script>
 
 <template>
-  <h2>Computed fullName - {{ fullName }}</h2>
-  <button @click="changeFullName">Change fullName</button>
+  <h2>Volume tracker (0-20)</h2>
+  <h3>Current volume - {{ volume }}</h3>
+  <div>
+    <button @click="volume += 2">Increase</button>
+    <button @click="volume -= 2">Decrease</button>
+  </div>
 </template>
 
 <style>
