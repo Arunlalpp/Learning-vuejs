30d4ba24aa807958b9da6dcf844529319c0a3009
Arunlal PP <arunlalpp@gmail.com>
2024-03-07 14:58:12 +0530

    feat: add compostion repo

---
+++ vue-composition/src/components/Data.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>options - {{ o_fristName }}</h2>
+  <h2>Composition - {{ c_firstName }}</h2>
+  <h2>Composition - {{ greet }}</h2>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  name: `Data`,
+  setup() {
+    const c_firstName = ref("PCP");
+    c_firstName.value = "PPC";
+    const greet = `Hello ${c_firstName.value}`;
+    console.log("c_firstName", c_firstName);
+    return {
+      c_firstName,
+      greet,
+    };
+  },
+  data() {
+    return {
+      o_fristName: "Arunlal",
+    };
+  },
+};
+</script>
+<style>
+</style>
