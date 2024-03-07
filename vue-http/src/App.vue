85cbfcb5a07d302846f8316b8e1caf7e899fa0eb
Arunlal PP <arunlalpp@gmail.com>
2024-03-07 14:03:19 +0530

    feat: vue refs

--- vue-http/src/App.vue
+++ vue-http/src/App.vue
@@ -1,22 +1,21 @@
 <template>
-<PostList />
+  <TemplateRef />
 </template>
 
 <script>
-import PostList from "./components/PostList.vue";
+import TemplateRef from "./components/TemplateRef.vue";
 
 export default {
   name: "App",
   components: {
-    PostList,
+    TemplateRef,
   },
 };
 </script>
 
 <style>
-
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
