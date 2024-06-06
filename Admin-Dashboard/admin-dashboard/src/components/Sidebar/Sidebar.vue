fc4968c7a3430b1cae461c6bd7e97b9926ccd332
Arunlal PP <arunlalpp@gmail.com>
2024-06-06 20:50:26 +0530

    feat: add side navbar menu items

--- Admin-Dashboard/admin-dashboard/src/components/Sidebar/Sidebar.vue
+++ Admin-Dashboard/admin-dashboard/src/components/Sidebar/Sidebar.vue
@@ -1,40 +1,67 @@
 <template>
-    <aside :class="{ 'is-expanded': is_expanded }">
+    <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
         <div class="logo">
-            <img :src="logo" alt="vue" />
+            <img :src="logo" alt="Vue" />
         </div>
+
         <div class="menu-toggle-wrap">
             <button class="menu-toggle" @click="ToggleMenu">
-                <span class="material-symbols-outlined">
-                    keyboard_double_arrow_right
-                </span>
+                <span class="material-icons">keyboard_double_arrow_right</span>
             </button>
+        </div>
+
+        <h3>Menu</h3>
+        <div class="menu">
+            <router-link class="button" to="/"><span class="material-symbols-outlined">home</span>
+                <span class="text">Home</span></router-link>
+            <router-link class="button" to="/about"><span class="material-symbols-outlined">visibility</span>
+                <span class="text">About</span></router-link>
+            <router-link class="button" to="/about"><span class="material-symbols-outlined">group</span>
+                <span class="text">Team</span></router-link>
+            <router-link class="button" to="/about"><span class="material-symbols-outlined">email</span>
+                <span class="text">Contact</span></router-link>
+        </div>
+
+        <div class="flex"></div>
+
+        <div class="menu">
+            <router-link to="/settings" class="button">
+                <span class="material-icons">settings</span>
+                <span class="text">Settings</span>
+            </router-link>
         </div>
     </aside>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import logo from '../../assets/vue.svg'
+import { ref } from "vue";
+import logo from "../../assets/vue.svg";
 
-const is_expanded = ref(false)
+const is_expanded = ref(false);
 
 const ToggleMenu = () => {
-    is_expanded.value = !is_expanded.value
-}
+    is_expanded.value = !is_expanded.value;
+};
 </script>
 
 <style lang="scss" scoped>
 aside {
     display: flex;
     flex-direction: column;
+
     background-color: var(--dark);
     color: var(--light);
+
     width: calc(2rem + 32px);
-    min-height: 100vh;
     overflow: hidden;
+    min-height: 100vh;
     padding: 1rem;
-    transition: width 0.2s ease-out;
+
+    transition: 0.2s ease-in-out;
+
+    .flex {
+        flex: 1 1 0%;
+    }
 
     .logo {
         margin-bottom: 1rem;
@@ -47,20 +74,93 @@ aside {
     .menu-toggle-wrap {
         display: flex;
         justify-content: flex-end;
-        position: relative;
         margin-bottom: 1rem;
+
+        position: relative;
         top: 0;
-        transition: top 0.2s ease-out;
+        transition: 0.2s ease-in-out;
 
         .menu-toggle {
-            transition: 0.2s ease-out;
-            border: none;
-            outline: none;
-        }
+            transition: 0.2s ease-in-out;
 
-        .material-symbols-outlined {
-            color: var(--light);
-            font-size: 2rem;
+            .material-symbols-outlined {
+                font-size: 2rem;
+                color: var(--dark);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .material-symbols-outlined {
+                    color: var(--primary);
+                    transform: translateX(0.5rem);
+                }
+            }
+        }
+    }
+
+    h3,
+    .button .text {
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+    }
+
+    h3 {
+        color: var(--grey);
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+        text-transform: uppercase;
+    }
+
+    .menu {
+        margin: 0 -1rem;
+
+        .button {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+
+            transition: 0.2s ease-in-out;
+            padding: 0.5rem 1rem;
+
+            .material-symbols-outlined {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-in-out;
+            }
+
+            .text {
+                color: var(--light);
+                transition: 0.2s ease-in-out;
+            }
+
+            &:hover {
+                background-color: var(--dark-alt);
+
+                .material-symbols-outlined,
+                .text {
+                    color: var(--primary);
+                }
+            }
+
+            &.router-link-exact-active {
+                background-color: var(--dark-alt);
+                border-right: 5px solid var(--primary);
+
+                .material-symbols-outlined,
+                .text {
+                    color: var(--primary);
+                }
+            }
+        }
+    }
+
+    .footer {
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+
+        p {
+            font-size: 0.875rem;
+            color: var(--grey);
         }
     }
 
@@ -74,10 +174,25 @@ aside {
                 transform: rotate(-180deg);
             }
         }
+
+        h3,
+        .button .text {
+            opacity: 1;
+        }
+
+        .button {
+            .material-symbols-outlined {
+                margin-right: 1rem;
+            }
+        }
+
+        .footer {
+            opacity: 0;
+        }
     }
 
-    @media (max-width: 768px) {
-        position: fixed;
+    @media (max-width: 1024px) {
+        position: absolute;
         z-index: 99;
     }
 }
