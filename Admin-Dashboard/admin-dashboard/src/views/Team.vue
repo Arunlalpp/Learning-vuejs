2dbdcb380e891827fed4eb0694e8320dd07ff6d8
Arunlal PP <arunlalpp@gmail.com>
2024-06-10 16:36:59 +0530

    feat: add input selector

--- Admin-Dashboard/admin-dashboard/src/views/Team.vue
+++ Admin-Dashboard/admin-dashboard/src/views/Team.vue
@@ -1,50 +1,72 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
-const firstName = ref('John')
-const lastName = ref('Doe')
 const header = ref('Shopping List App')
+const newItem = ref('')
+const isHighPriority = ref(false)
+
 const items = ref([
     { id: 1, label: 'Sample1' },
     { id: 2, label: 'Sample2' },
     { id: 3, label: 'Sample3' },
 ])
 
-const fullName = computed({
-    // getter
-    get() {
-        return firstName.value + ' ' + lastName.value
-    },
-    // setter
-    set(newValue) {
-        // Note: we are using destructuring assignment syntax here.
-        [firstName.value, lastName.value] = newValue.split(' ')
+const handleAddItems = () => {
+    if (newItem.value.trim() !== '') {
+        items.value.push({ id: items.value.length + 1, label: newItem.value })
+        newItem.value = ''
+        isHighPriority.value = false
     }
-})
+}
 </script>
 
 <template>
     <div class="container">
         <h1>{{ header }}</h1>
-
+        <div class="wrapperContainer">
+            <input type="text" v-model.trim="newItem" placeholder="Add new item">
+            <label>
+                <input type="checkbox" v-model="isHighPriority">
+                High Priority
+            </label>
+            <button @click="handleAddItems">Save Item</button>
+        </div>
         <ul>
-            <li v-for="{ id, label }, items in items" :key="id">{{ label }}</li>
+            <li v-for="item in items" :key="item.id">{{ item.label }}</li>
         </ul>
     </div>
 </template>
 
 <style scoped>
 .container {
-    width: 300px;
+    width: 450px;
     height: 230px;
     background-color: moccasin;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
     gap: 10px;
+    padding: 20px;
+    margin: 10px;
+}
 
-    ul li {
-        list-style-type: none;
-    }
+ul li {
+    list-style-type: none;
+}
+
+.wrapperContainer {
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    align-items: center;
+}
+
+button {
+    padding: 10px;
+    outline: none;
+    border: none;
+    background-color: blanchedalmond;
+    border-radius: 8px;
+    cursor: pointer;
 }
 </style>
