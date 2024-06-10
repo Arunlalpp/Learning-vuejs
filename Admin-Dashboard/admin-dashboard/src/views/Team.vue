aeb11b528f832825c5a5920d5d8aeea7cc36ca6f
Arunlal PP <arunlalpp@gmail.com>
2024-06-10 12:59:12 +0530

    feat: add v-for mapping with destructuring

--- Admin-Dashboard/admin-dashboard/src/views/Team.vue
+++ Admin-Dashboard/admin-dashboard/src/views/Team.vue
@@ -3,6 +3,12 @@ import { ref, computed } from 'vue'
 
 const firstName = ref('John')
 const lastName = ref('Doe')
+const header = ref('Shopping List App')
+const items = ref([
+    { id: 1, label: 'Sample1' },
+    { id: 2, label: 'Sample2' },
+    { id: 3, label: 'Sample3' },
+])
 
 const fullName = computed({
     // getter
@@ -18,5 +24,27 @@ const fullName = computed({
 </script>
 
 <template>
-    <p>{{ fullName }}</p>
+    <div class="container">
+        <h1>{{ header }}</h1>
+
+        <ul>
+            <li v-for="{ id, label }, items in items" :key="id">{{ label }}</li>
+        </ul>
+    </div>
 </template>
+
+<style scoped>
+.container {
+    width: 300px;
+    height: 230px;
+    background-color: moccasin;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+
+    ul li {
+        list-style-type: none;
+    }
+}
+</style>
