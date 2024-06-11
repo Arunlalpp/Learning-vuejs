6c8fa014597480aa58f5bb6979d5564a25d84b67
Arunlal PP <arunlalpp@gmail.com>
2024-06-11 16:17:54 +0530

    feat: add haeder compoennts

---
+++ learning-nuxt/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="container">
+        <WrapperContainer>
+            <nav>
+                <div class="logo">logo</div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+        </WrapperContainer>
+    </header>
+</template>
---
+++ learning-nuxt/pages/events/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+console.log(route.params.id);
+</script>
+
+<template>
+    <div>event id:{{ $route.params.id }}</div>
+</template>
