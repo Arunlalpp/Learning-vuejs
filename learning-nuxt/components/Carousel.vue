032e39d57472d3d7731088e4ed713eac661ca4b5
Arunlal PP <arunlalpp@gmail.com>
2024-06-12 09:29:56 +0530

    feat: add carousel image container

--- learning-nuxt/components/Carousel.vue
+++ learning-nuxt/components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="carousel">
+  <div class="carousel" ref="carouselDom">
     <div class="list" ref="sliderDom">
       <CarouselItem v-for="(item, index) in items" :key="index" :image="item.image" :author="item.author"
         :title="item.title" :topic="item.topic" :description="item.description" />
@@ -20,36 +20,42 @@ import { ref, onMounted, onBeforeUnmount } from 'vue'
 import CarouselItem from '~/components/CarouselItem.vue'
 import Thumbnail from '~/components/Thumbnail.vue'
 
-const items = ref([
+const dummySliderData = [
   {
-    image: 'https://images.unsplash.com/photo-1718030323555-214805b7f884?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    author: 'LUNDEV',
-    title: 'DESIGN SLIDER',
-    topic: 'ANIMAL',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit...'
+    image:
+      "https://images.unsplash.com/photo-1718030323555-214805b7f884?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "LUNDEV",
+    title: "DESIGN SLIDER",
+    topic: "ANIMAL",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
   },
   {
-    image: 'https://images.unsplash.com/photo-1707243103665-1de6cbf8f66f?q=80&w=1493&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    author: 'LUNDEV',
-    title: 'DESIGN SLIDER',
-    topic: 'ANIMAL',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit...'
+    image:
+      "https://images.unsplash.com/photo-1707243103665-1de6cbf8f66f?q=80&w=1493&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "LUNDEV",
+    title: "DESIGN SLIDER",
+    topic: "ANIMAL",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
   },
   {
-    image: 'https://images.unsplash.com/photo-1612967335954-cb297626daa7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    author: 'LUNDEV',
-    title: 'DESIGN SLIDER',
-    topic: 'ANIMAL',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit...'
+    image:
+      "https://images.unsplash.com/photo-1612967335954-cb297626daa7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "LUNDEV",
+    title: "DESIGN SLIDER",
+    topic: "ANIMAL",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
   },
   {
-    image: 'https://images.unsplash.com/photo-1588854337221-4cf9fa96059c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    author: 'LUNDEV',
-    title: 'DESIGN SLIDER',
-    topic: 'ANIMAL',
-    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit...'
-  }
-])
+    image:
+      "https://images.unsplash.com/photo-1588854337221-4cf9fa96059c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    author: "LUNDEV",
+    title: "DESIGN SLIDER",
+    topic: "ANIMAL",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit...",
+  },
+];
+
+const items = ref(dummySliderData)
 
 const carouselDom = ref(null)
 const sliderDom = ref(null)
