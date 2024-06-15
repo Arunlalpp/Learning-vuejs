8b63f220540f1af1accb730fd9ad9833a6346d78
Arunlal PP <arunlalpp@gmail.com>
2024-06-15 11:05:55 +0530

    refactor: code change

--- learning-nuxt/pages/navlinks/About.vue
+++ learning-nuxt/pages/navlinks/About.vue
@@ -1,28 +1,10 @@
-<script setup>
-definePageMeta({
-  pageTransition: { name: "rotate" },
-  layout: 'CustomLayout'
-
-})
-
-</script>
-
 <template>
-  <div style="background-color: aqua;">
-    <h1>About page</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+  <div>
+      About
   </div>
 </template>
+<script>
+</script>
+<style lang="">
 
-<style scoped>
-.rotate-enter-active,
-.rotate-leave-active {
-  transition: all 0.4s;
-}
-
-.rotate-enter-from,
-.rotate-leave-to {
-  opacity: 0;
-  transform: rotate3d(1, 1, 1, 15deg);
-}
 </style>
--- learning-nuxt/pages/navlinks/LoginAndRegister.vue
+++ learning-nuxt/pages/navlinks/LoginAndRegister.vue
@@ -72,6 +72,7 @@ export default {
 };
 
 definePageMeta({
-    layout: false
+    layout: false,
+
 })
 </script>
